e0a82bd042a97974afb050796183b1d23b67a270
3ylam <aylamz2002@gmail.com>
2022-04-14 18:06:29 -0700

    starting lesson 8

--- 8.Dart_Programming_Loops.dart
+++ 8.Dart_Programming_Loops.dart
@@ -7,14 +7,37 @@
                                       *# Loop in Dar  #*
                                       
 - At times, certain certain instructions require repeated execution.
-- 
+- Loops are an ideal way to do the same. 
+- A loop represents a set of instructions that must be repeated.
+- In a loop’s context, a repetition is termed as an iteration.
 
+# What is loop means ?
+  - 
+  -
+  -
 
 
 
 */
 void main(List<String> args) {
+
   print("Line 18 output => " + "Hello there!");
   print("Line 19 output => " + "From Lesson -8-");
   print("Line 20 output => " + "Loop in Dart");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
